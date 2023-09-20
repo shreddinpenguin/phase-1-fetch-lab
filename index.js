461fef0ffe6251d7f5b26b4129cb9d24f9f1e5bc
Taylor Riley <t_riley13@icloud.com>
2023-09-19 20:48:56 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(json => renderBooks(json))
 }
-
+fetchBooks()
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
